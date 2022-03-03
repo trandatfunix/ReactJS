f5cd9e8aa31172b2e1d9d8a7c785eda22eb200cb
trandatfunix <datttfx13380@funix.edu.vn>
2022-03-03 14:43:55 +0700

    Lab 02.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 class App extends Component {
-
   render() {
     return (
       <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
+        <h1>Hello - This is lab 02.1</h1>
       </div>
     );
   }
